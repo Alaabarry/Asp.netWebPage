30028de873b357c9391f8981c4aa81dde5827780
unknown <zerohand99@gmail.com>
2023-05-02 22:24:47 +0300

    built a website

---
+++ WebApp/Scripts/master.js
@@ -0,0 +1,10 @@
+﻿var text = "Welcome to Tech Club...";
+var x = 0;
+function typing() {
+    if (x < text.length) {
+        document.getElementById("test").innerHTML += text.charAt(x);
+        x++;
+        setTimeout(typing, 250);
+    }
+}
+typing();
